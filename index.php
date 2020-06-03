caa6d81295027defbfbadd9b6bae3e0e419d0783
aroca <alexander.roca@actecperu.com>
2020-06-03 12:00:16 -0500

    mensaje de error

--- index.php
+++ index.php
@@ -12,31 +12,95 @@ fwrite($fp, $texto, 4); // Escribirá sólo: Hola
 // hasta ocupar todo el texto nuevo, si no hay mas texto despues del que deseas reemplazar
 // este si el texto nuevo es mayor este se añadira en nuevas lineas o espacios.
 
-  $fh   = fopen("file_prueba.php", 'r+') or die("Ocurrio un error al abrir el archivo");
-  $texto = fgets($fh);
+//el @ es para ignorar el mensaje de error del servidor
+
+  //$fh   = @fopen("fi le_prueba.php", 'r+') or die("Ocurrio un error al abrir el archivo");
+  $fh   = @fopen("file_prueba.php", 'r+');
   
-  //fseek($fh, 0, SEEK_END);
-  fseek($fh, 3372, SEEK_SET);
-  fwrite($fh, "'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'lwrwpn4e_demov',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => 'tec_',
-	'pconnect' => FALSE,
-	'db_debug' => FALSE,
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => FALSE
-);") or die("No se puede escribir en el archivo");
-  fclose($fh);  
-  
-  echo "Modificado correctamente";
+  /*if(!$fh){
+	  echo "no se pudo Modificar file";
+  }else{*/
+	  //$texto = @fgets($f);
+	  //fseek($fh, 0, SEEK_END);
+	  @fseek($fh, 3437, SEEK_SET);
+	  /*@fwrite($fh, "'hostname' => 'localhost',
+		'username' => 'root',
+		'password' => '123456789',
+		'database' => 'lwrwpn4e_demov',
+		'dbdriver' => 'mysqli',
+		'dbprefix' => 'tec_',
+		'pconnect' => FALSE,
+		'db_debug' => FALSE,
+		'cache_on' => FALSE,
+		'cachedir' => '',
+		'char_set' => 'utf8',
+		'dbcollat' => 'utf8_general_ci',
+		'swap_pre' => '',
+		'encrypt' => FALSE,
+		'compress' => FALSE,
+		'stricton' => FALSE,
+		'failover' => array(),
+		'save_queries' => FALSE
+		);") or die("No se puede escribir en el archivo");
+	  fclose($fh); */
+	  
+	  if( @fwrite($fh, "'hostname' => 'localhost',
+		'username' => 'root',
+		'password' => '123456789',
+		'database' => 'lwrwpn4e_demov',
+		'dbdriver' => 'mysqli',
+		'dbprefix' => 'tec_',
+		'pconnect' => FALSE,
+		'db_debug' => FALSE,
+		'cache_on' => FALSE,
+		'cachedir' => '',
+		'char_set' => 'utf8',
+		'dbcollat' => 'utf8_general_ci',
+		'swap_pre' => '',
+		'encrypt' => FALSE,
+		'compress' => FALSE,
+		'stricton' => FALSE,
+		'failover' => array(),
+		'save_queries' => FALSE
+		);")){// or die("No se puede escribir en el archivo")){
+			
+		  echo "Modificado correctamente";
+		  fclose($fh);
+		  
+	  }else{
+		  
+		  echo "error al Modificar el file";
+		  
+	  }
+	  
+	  
+//}
+		
+/*		
+$file = "file_prueb.php";
+$gestor = @fopen($file, "r+");
+$contador=0;
+if(!$gestor){
+	echo "no se pudo leer archivo";
+}else{
+	while (($búfer[$contador] = fgets($gestor, 4096)) !== false) {
+			//echo $búfer[$contador]."<br>";
+			//$contenido[$contador]=$búfer[$contador];
+			$contador++;
+		}
+
+	  
+
+	for($i=0; $i<$contador; $i++){
+		 echo $i." - ".$búfer[$i];
+		 echo "<br>";
+	}
+
+
+
+	fclose($gestor);//cierras el fichero 
+}
+*/
+
+
 
